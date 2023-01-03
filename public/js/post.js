cb9b3fe85e87ebf25e4cbf70602040ba8e84e2ed
JaredIng87 <jaredingersoll14@gmail.com>
2023-01-03 17:31:40 -0500

    create public folder with style sheet/javascript

---
+++ public/js/post.js
@@ -0,0 +1,27 @@
+const createPostHandler = async (event) => {
+  event.preventDefault();
+
+
+  const title = document.querySelector('#post-title').value.trim();
+  const text = document.querySelector('#post-text').value.trim();
+
+
+
+  if (title && text) {
+    const response = await fetch('/api/blog/add', {
+      method: 'POST',
+      body: JSON.stringify({ title, text }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/memberProfile');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document
+  .querySelector('.create-post')
+  .addEventListener('submit', createPostHandler);
